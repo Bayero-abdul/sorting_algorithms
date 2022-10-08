5b2c860f3aa9888c5e614ab7aaf13fe0d0a49ddd
Bayero-abdul <bayeroa65@gmail.com>
2022-10-08 10:02:00 +0000

    Sorts a doubly linked list of ints in using the Insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts list in ascending order
+ * @list: list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp = NULL, *walk = NULL, *next = NULL, *next_key = NULL;
+
+	if (*list == NULL || (*list)->next == NULL)
+		return;
+
+	walk = (*list)->next;
+	next_key = walk->next;
+	while (walk)
+	{
+		next = walk->next;
+		temp = walk->prev;
+		while (temp && walk->n < temp->n)
+		{
+			walk->next = temp;
+			walk->prev = temp->prev;
+			if (temp->prev)
+				temp->prev->next = walk;
+
+			temp->prev = walk;
+			temp->next = next;
+			if (temp->next)
+				temp->next->prev = temp;
+
+			if (walk->prev == NULL)
+				*list = walk;
+			print_list(*list);
+			temp = walk->prev;
+			next = walk->next;
+		}
+		walk = next_key;
+		if (walk)
+			next_key = walk->next;
+	}
+}

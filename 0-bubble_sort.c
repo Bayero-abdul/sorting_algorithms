c4ae2e2d269036dcf30d27ffdd5d35235eb5e80a
Bayero-abdul <bayeroa65@gmail.com>
2022-10-09 19:15:13 +0000

    Test code

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,16 +15,16 @@ void bubble_sort(int *array, size_t size)
 	if (size < 2)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		swapped = 0;
 		for (j = 1; j < size - i; j++)
 		{
 			if (array[j - 1] > array[j])
 			{
-				temp = array[j - 1];
-				array[j - 1] = array[j];
-				array[j] = temp;
+				temp = array[j];
+				array[j] = array[j - 1];
+				array[j - 1] = temp;
 				print_array(array, size);
 				swapped = 1;
 			}

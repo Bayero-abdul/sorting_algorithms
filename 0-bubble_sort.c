1794f094d5ac376f00baf8cab9d4cd144f244fdb
Bayero-abdul <bayeroa65@gmail.com>
2022-10-09 19:08:00 +0000

    Refactor code

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,7 +15,7 @@ void bubble_sort(int *array, size_t size)
 	if (size < 2)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		swapped = 0;
 		for (j = 1; j < size - i; j++)

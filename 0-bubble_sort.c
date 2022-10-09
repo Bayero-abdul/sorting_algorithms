b68cfd07a6d67fe0066aaade940fea78fba71501
Bayero-abdul <bayeroa65@gmail.com>
2022-10-09 19:23:48 +0000

    Revert "Test code" This reverts commit 89713802871c8a99e72721e052db8adf1fd9f1b1.

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,16 +15,16 @@ void bubble_sort(int *array, size_t size)
 	if (size < 2)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		swapped = 0;
-		for (j = 0; j < size - 1 - i; j++)
+		for (j = 1; j < size - i; j++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[j - 1] > array[j])
 			{
 				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				array[j] = array[j - 1];
+				array[j - 1] = temp;
 				print_array(array, size);
 				swapped = 1;
 			}

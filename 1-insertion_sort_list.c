3ab84a6158dbcb8630673ed294c80cae079dc708
Bayero-abdul <bayeroa65@gmail.com>
2022-10-11 10:43:00 +0000

    Test code

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,7 +8,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *temp = NULL, *walk = NULL, *next = NULL;
 
-	if (*list == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	walk = (*list)->next;
